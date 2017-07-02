c7493e637e88a9ce3f11571e146a1dbca49181b4
william1638 <yzw1638@outlook.com>
2017-07-02 10:58:46 +0800

    update 用户模块 商品分类模块

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 mall learn
+
+接口文档
+http://git.oschina.net/imooccode/happymmallwiki/wikis/%E9%97%A8%E6%88%B7_%E7%94%A8%E6%88%B7%E6%8E%A5%E5%8F%A3
---
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -0,0 +1,20 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
+
+/**
+ * Created by William on 2017/7/2.
+ */
+public interface ICategoryService {
+
+    ServerResponse<String> addCategory(String categoryName, Integer parentId);
+
+    ServerResponse<String> updateCategoryName(Integer categoryId,String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse selectCategoryAndChildrenById(Integer categoryId);
+}
